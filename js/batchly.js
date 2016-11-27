4692ab8d63e40631376e63e99e3dbcb8d9a7e0aa
Rahul Prasad <rahul@47line.com>
2016-11-27 11:16:06 +0530

    New version of batchly marketing site

--- js/batchly.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-  $('.hamburger').on('click',function(event){
-      event.preventDefault();
-      $('.batchly-menu').fadeToggle();
-  });
-});
---
+++ js/case-studies-slider.js
@@ -0,0 +1,2 @@
+
+(function($){$(".case-studies-slider-list").slick({arrows:false,dots:true});})(jQuery);
